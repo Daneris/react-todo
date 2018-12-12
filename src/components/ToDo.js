eee5b3dc4c50c8a9e6e0254bfd255a63cd4f4f36
Daneris Cruz <daneriscruz@Daneriss-MacBook-Air.local>
2018-12-12 13:12:26 -0500

    created ToDo.js file and outline of ToDo class

---
+++ src/components/ToDo.js
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+
+class ToDo extends Component {
+  render() {
+
+    return(
+      <li>A Todo will go here</li>
+
+    )
+  }
+
+
+
+}
+
+export default ToDo;
